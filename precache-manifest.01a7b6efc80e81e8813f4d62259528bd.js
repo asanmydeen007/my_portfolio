d3caec60ae95a6bfc7a3c5370d79da4b2d9a2736
Asan Mydeen <asan@codingmart.com>
2022-09-21 20:51:37 +0530

    Updates

---
+++ precache-manifest.01a7b6efc80e81e8813f4d62259528bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb01039599acd9c6f6012b0b3ecb0af5",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "e8692a136f515c40088c",
+    "url": "/my_portfolio/static/css/2.3aee7458.chunk.css"
+  },
+  {
+    "revision": "6d3c0baec666aeeb7e53",
+    "url": "/my_portfolio/static/css/main.332ea96c.chunk.css"
+  },
+  {
+    "revision": "e8692a136f515c40088c",
+    "url": "/my_portfolio/static/js/2.27c5a6dc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/my_portfolio/static/js/2.27c5a6dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d3c0baec666aeeb7e53",
+    "url": "/my_portfolio/static/js/main.bc5dae97.chunk.js"
+  },
+  {
+    "revision": "1107bcf17560ca4fe97c",
+    "url": "/my_portfolio/static/js/runtime-main.b6edbb39.js"
+  }
+]);
